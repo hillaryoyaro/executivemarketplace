edb8f6c7a01f4c3f44acdce14139b1dc02008290
hillaryoyaro <41636593+hillaryoyaro@users.noreply.github.com>
2025-07-17 17:32:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/home/Header/Header.js
@@ -0,0 +1,193 @@
+import React, { useEffect, useRef,useState } from "react";
+import { Link, NavLink, useLocation } from "react-router-dom";
+import { MdClose } from "react-icons/md";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { HiOutlineMenuAlt4 } from "react-icons/hi";
+import { motion } from "framer-motion";
+import { logo, logoLight } from "../../../assets/images";
+import Image from "../../designLayouts/Image";
+import { navBarList } from "../../../constants";
+import Flex from "../../designLayouts/Flex";
+
+const Header = () => {
+  const [show, setShow] = useState(false);
+  const [showUser, setShowUser] = useState(false);
+  const ref = useRef();
+  const [showMenu, setShowMenu] = useState(true);
+  const [sidenav, setSidenav] = useState(false);
+  const [category, setCategory] = useState(false);
+  const [brand, setBrand] = useState(false);
+  const location = useLocation();
+  useEffect(() => {
+    let ResponsiveMenu = () => {
+      if (window.innerWidth < 667) {
+        setShowMenu(false);
+      } else {
+        setShowMenu(true);
+      }
+    };
+    ResponsiveMenu();
+    window.addEventListener("resize", ResponsiveMenu);
+  }, []);
+
+  return (
+    <div className="w-full h-20   bg-[#e9f1fe] border-b-[1px] ">
+      <nav className="h-full px-4 max-w-container mx-auto relative">
+        <Flex className="flex items-center justify-between h-full">
+                  <div
+                    onClick={() => setShow(!show)}
+                    ref={ref}
+                    className="flex h-14 cursor-pointer items-center gap-2 text-primeColor"
+                  >
+                    <HiOutlineMenuAlt4 className="w-5 h-5" />
+                    <p className="text-[14px] font-normal">Shop by Amazing</p>
+        
+                    {show && (
+                      <motion.ul
+                        initial={{ y: 30, opacity: 0 }}
+                        animate={{ y: 0, opacity: 1 }}
+                        transition={{ duration: 0.5 }}
+                        className="absolute top-36 z-50 bg-primeColor w-auto text-[#767676] h-auto p-4 pb-6"
+                      >
+                        <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer">
+                          Accessories
+                        </li>
+                        <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer">
+                          Furniture
+                        </li>
+                        <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer">
+                          Electronics
+                        </li>
+                        <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400 hover:border-b-white hover:text-white duration-300 cursor-pointer">
+                          Clothes
+                        </li>
+                        <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400  hover:border-b-white hover:text-white duration-300 cursor-pointer">
+                          Bags
+                        </li>
+                        <li className="text-gray-400 px-4 py-1 border-b-[1px] border-b-gray-400  hover:border-b-white hover:text-white duration-300 cursor-pointer">
+                          Home appliances
+                        </li>
+                      </motion.ul>
+                    )}
+                  </div>
+          
+          <div>
+            {showMenu && (
+              <motion.ul
+                initial={{ y: 30, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ duration: 0.5 }}
+                className="flex items-center w-auto z-50 p-0 gap-2"
+              >
+                <>
+                  {navBarList.map(({ _id, title, link }) => (
+                    <NavLink
+                      key={_id}
+                      className="flex font-normal hover:font-bold w-20 h-6 justify-center items-center px-12 text-white  hover:underline underline-offset-[4px] decoration-[1px] hover:text-[#262626] md:border-r-[2px] border-r-gray-300 hoverEffect last:border-r-0"
+                      to={link}
+                      state={{ data: location.pathname.split("/")[1] }}
+                    >
+                      <li>{title}</li>
+                    </NavLink>
+                  ))}
+                </>
+              </motion.ul>
+            )}
+            <HiMenuAlt2
+              onClick={() => setSidenav(!sidenav)}
+              className="inline-block md:hidden cursor-pointer w-8 h-6 absolute top-6 right-4"
+            />
+            {sidenav && (
+              <div className="fixed top-0 left-0 w-full h-screen bg-black text-gray-200 bg-opacity-80 z-50">
+                <motion.div
+                  initial={{ x: -300, opacity: 0 }}
+                  animate={{ x: 0, opacity: 1 }}
+                  transition={{ duration: 0.5 }}
+                  className="w-[80%] h-full relative"
+                >
+                  <div className="w-full h-full bg-primeColor p-6">
+                    <img
+                      className="w-28 mb-6"
+                      src={logoLight}
+                      alt="logoLight"
+                    />
+                    <ul className="text-black flex flex-col gap-2">
+                      {navBarList.map((item) => (
+                        <li
+                          className="font-normal hover:font-bold items-center text-lg text-black hover:underline underline-offset-[4px] decoration-[1px] hover:text-blue md:border-r-[2px] border-r-gray-300 hoverEffect last:border-r-0"
+                          key={item._id}
+                        >
+                          <NavLink
+                            to={item.link}
+                            state={{ data: location.pathname.split("/")[1] }}
+                            onClick={() => setSidenav(false)}
+                          >
+                            {item.title}
+                          </NavLink>
+                        </li>
+                      ))}
+                    </ul>
+                    <div className="mt-4">
+                      <h1
+                        onClick={() => setCategory(!category)}
+                        className="flex justify-between text-base cursor-pointer items-center font-titleFont mb-2"
+                      >
+                        Shop by Category{" "}
+                        <span className="text-lg">{category ? "-" : "+"}</span>
+                      </h1>
+                      {category && (
+                        <motion.ul
+                          initial={{ y: 15, opacity: 0 }}
+                          animate={{ y: 0, opacity: 1 }}
+                          transition={{ duration: 0.4 }}
+                          className="text-sm flex flex-col gap-1"
+                        >
+                          <li className="headerSedenavLi">New Arrivals</li>
+                          <li className="headerSedenavLi">Gudgets</li>
+                          <li className="headerSedenavLi">Accessories</li>
+                          <li className="headerSedenavLi">Electronics</li>
+                          <li className="headerSedenavLi">Others</li>
+                        </motion.ul>
+                      )}
+                    </div>
+                    <div className="mt-4">
+                      <h1
+                        onClick={() => setBrand(!brand)}
+                        className="flex justify-between text-base cursor-pointer items-center font-titleFont mb-2"
+                      >
+                        Shop by Brand
+                        <span className="text-lg">{brand ? "-" : "+"}</span>
+                      </h1>
+                      {brand && (
+                        <motion.ul
+                          initial={{ y: 15, opacity: 0 }}
+                          animate={{ y: 0, opacity: 1 }}
+                          transition={{ duration: 0.4 }}
+                          className="text-sm flex flex-col gap-1"
+                        >
+                          <li className="headerSedenavLi">New Arrivals</li>
+                          <li className="headerSedenavLi">Gudgets</li>
+                          <li className="headerSedenavLi">Accessories</li>
+                          <li className="headerSedenavLi">Electronics</li>
+                          <li className="headerSedenavLi">Others</li>
+                        </motion.ul>
+                      )}
+                    </div>
+                  </div>
+                  <span
+                    onClick={() => setSidenav(false)}
+                    className="w-8 h-8 border-[1px] border-gray-300 absolute top-2 -right-10 text-gray-300 text-2xl flex justify-center items-center cursor-pointer hover:border-red-500 hover:text-red-500 duration-300"
+                  >
+                    <MdClose />
+                  </span>
+                </motion.div>
+              </div>
+            )}
+          </div>
+        </Flex>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
